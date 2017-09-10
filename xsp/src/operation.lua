7a1c6123a2f65a973857b3453e3a06fd0e350306
Anthony <cdistc@163.com>
2017-09-10 22:50:11 +0800

    fix some bugs

--- xsp/src/operation.lua
+++ xsp/src/operation.lua
@@ -42,7 +42,13 @@ function getOperation(height,width)
   operation.nextLevel=common.clickButton;
   
   operation.keepRune=common.clickButton;
-  operation.saleFiveStarRune=common.clickButton;
+  operation.saleFiveStarRune=function(param)
+		local yes={0xf8e9ad,"0|2|0x482f09,2|7|0xf8e9ad,2|12|0x412702,409|9|0xf8e9ad,400|9|0x412702,386|12|0xcfa349", 95, 778, 865, 860, 935};
+		local flag=common.clickButton(yes);
+    while flag==false do
+      flag=common.clickButton(yes);
+    end;
+	end;
   
   operation.defeat=empty;
   
@@ -78,7 +84,7 @@ function getOperation(height,width)
   end
   
   operation.notEnoughEnergyBuy=function(param)
-    local energy={0xffcc66,"-25|17|0xffcc66,19|-121|0xffffdd,484|-497|0xf7efdf,486|-489|0x40250d,484|-463|0x40250d,490|-341|0x4ab2f4,513|-330|0x003b8f,915|-342|0xdc1c4d", 95, 482, 735, 529, 772};
+    local energy={0xffcc66,"-32|316|0xdb1b4b,448|9|0xffffff,486|-315|0x3ea5f2,95|-400|0x482e1f,-24|-354|0x482e1f,95|-11|0xe2c582,-11|-186|0xf7ecc2", 95, 422, 660, 593, 812};
     local buy={0xf8e9ad,"0|3|0x412702,0|7|0xf8e9ad,4|-30|0xdeb75d,95|-267|0xf8e9ad,107|-267|0x482e1f,115|-267|0xf8e9ad,612|-477|0xa7163a,190|-472|0x2b7ab7", 95, 791, 877, 854, 919};
     local done={0x412702,"6|-4|0xf8e9ad,-19|-264|0xe2c582,-20|-269|0x482e1f,-16|-272|0xe2c582,-15|-276|0x482e1f,-4|-469|0x295f81,427|-478|0x730e27", 95, 951, 860, 1098, 921};
     local close={0xf8f0cf,"8|-7|0xb98c4a,9|-10|0x412702,10|-14|0xf8f0cf,-14|-163|0xd81847,-518|-160|0xe32253,495|-164|0xd81948,541|-461|0x44b8ff", 95, 971, 1194, 1075, 1245};
@@ -93,24 +99,30 @@ function getOperation(height,width)
       delta=os.time()-startTime;
     end;
 		--点体力
-    flag=common.clickButton(energy);
+    --flag=common.clickButton(energy);
+		sysLog('test');
+		flag=false;
     while flag==false and delta<timeout do
       flag=common.clickButton(energy);
+			sysLog(tostring(flag));
       delta=os.time()-startTime;
     end;		
 		--确认购买
-    flag=common.clickButton(buy);
+    --flag=common.clickButton(buy);
+		flag=false;
     while flag==false and delta<timeout do
       flag=common.clickButton(buy);
       delta=os.time()-startTime;
     end;		--点购买完成
-		flag=common.clickButton(done);
+		--flag=common.clickButton(done);
+		flag=false;
     while flag==false and delta<timeout do
       flag=common.clickButton(done);
       delta=os.time()-startTime;
     end;
 		--点关闭
-		flag=common.clickButton(close);
+		--flag=common.clickButton(close);
+		flag=false;
     while flag==false and delta<timeout do
       flag=common.clickButton(close);
       delta=os.time()-startTime;
