e6d8b598c5bb3fe4d307b8e221c890a698baf38d
Anthony <cdistc@163.com>
2018-10-07 20:27:35 +0800

    1

---
+++ xsp/src/points/mix2.lua
@@ -0,0 +1,113 @@
+mix2={
+	--开始战斗的按钮
+	startFight={{1589, 669, 1856, 891},
+		"0|0|0xf6e7ab,1|13|0xf7e8ac,9|27|0xc29037,29|35|0xc18d36,42|32|0xb0a57b,46|33|0x412702,67|20|0xe3d69f,88|29|0x67522a,113|10|0xf8e9ad,137|12|0xf8e9ad",
+		95, 0, 0, 0},
+	
+	--自动战斗得三角形
+	tangle={},
+	
+	--战斗齿轮
+	gear={},
+	
+	--胜利和rgb
+	victoryRGB={},
+	
+	--胜利标记
+	victory={},
+	
+	--RGB标记
+	rgb={},
+	
+	--获得奖励确认
+	confirmReward={},
+	
+	--出售符文
+	saleRune={},
+	
+	--确认出售符文
+	confirmSaleRune={},
+	
+	--保留符文
+	keepRune={},
+	
+	--再来一次
+	again={},
+	
+	--死了不复活
+	noRevive={},
+	
+	--失败+rgb
+	defeat={},
+	
+	--准备战斗
+	prepareBattle={},
+	
+	--打开商店
+	openStore={},
+	
+	--点击体力
+	clickEnerge={},
+	
+	--确认购买体力,按钮是
+	buyEnerge={},
+	
+	--购买成功确认
+	finishBuyConfirm={},
+	--关闭商店
+	closeStore={},
+	
+	--小boss
+	littleBoss={},
+	
+	bigBoss={},
+	
+	nextStage={
+	},
+	
+	-----合成
+	--合成按钮
+	synthesis={
+	},
+	--确定合成
+	confirmSynthesis={
+	},
+	--合成成功
+	synthesisOk={
+	},
+	
+	-----
+	pureDefeat={
+	},
+	
+	--三星碎片 rb 聊天标记
+	rbTalk={
+	},
+	
+	--异界相关
+	--队长 开始战斗的按钮
+	owStartFight={
+	},
+	
+	--确认开始战斗
+	owConfirmStartFight={
+	},
+	--战斗齿轮
+	owGear={
+	},
+	--胜利标记
+	owVictory={
+	},
+	owDefeat={
+	},
+	owSameTeamAgain={
+	},
+	
+	--队员准备战斗
+	owReadyToFight={
+	}
+	
+}
+
+
+return mix2
